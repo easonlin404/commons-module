4bc5ff501524355608d61468102524645f90edfd
easonlin win7 <easonlin404@gmail.com>
2016-01-22 11:49:55 +0800

    update

---
+++ spring-web/src/main/java/soundsystem/CDPlayerConfig.java
@@ -0,0 +1,27 @@
+package soundsystem;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CDPlayerConfig {
+
+	@Bean(name = "lonelyHeartsClubBand")
+	public CompactDisc sgtPeppers() {
+		return new SgtPeppers();
+	}
+
+	@Bean
+	public CompactDisc randomBeatlesCD() {
+		int choice = (int) Math.floor(Math.random() * 4);
+		if (choice == 0) {
+			return new SgtPeppers();
+		} else if (choice == 1) {
+			return new WhiteAlbum();
+		} else if (choice == 2) {
+			return new HardDaysNight();
+		} else {
+			return new Revolver();
+		}
+	}
+}
