f8138663605c144de6332cb76bdfbe97a7b3b22c
easonlin win7 <easonlin404@gmail.com>
2016-01-30 11:10:09 +0800

    change to around annotaion

--- spring-web/src/main/java/concert/Audience.java
+++ spring-web/src/main/java/concert/Audience.java
@@ -1,34 +1,27 @@
 package concert;
 
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class Audience {
-	
+
 	@Pointcut("execution(** concert.Performance.perform(..))")
-	public void performance() {}
-	
-	@Before("performance()")
-	public void silenceCellPhones() {
-		System.out.println("Silencing cell phones");
+	public void performance() {
 	}
 
-	@Before("performance()")
-	public void takeSeats() {
-		System.out.println("Taking seats");
-	}
-
-	@AfterReturning("performance()")
-	public void applause() {
-		System.out.println("CLAP CLAP CLAP!!!");
-	}
-
-	@AfterThrowing("performance()")
-	public void demandRefund() {
-		System.out.println("Demanding a refund");
+	@Around("performance()")
+	public void watchPerformance(ProceedingJoinPoint jp) throws Throwable {
+		try {
+			System.out.println("Silencing cell phones");
+			System.out.println("Taking seats");
+			jp.proceed();
+			System.out.println("CLAP CLAP CLAP!!!");
+		} catch (Throwable e) {
+			System.out.println("Demanding a refund");
+			throw e;
+		}
 	}
 }
