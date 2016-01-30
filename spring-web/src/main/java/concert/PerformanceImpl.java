b5abcd236eea12288c6b6b1ec6e9cefae0ee27ab
easonlin win7 <easonlin404@gmail.com>
2016-01-30 09:31:25 +0800

    update

---
+++ spring-web/src/main/java/concert/PerformanceImpl.java
@@ -0,0 +1,13 @@
+package concert;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PerformanceImpl implements Performance{
+
+	@Override
+	public void perform() {
+		System.out.println("PerformanceImpl.perform()");
+	}
+
+}
