a986d6acefe460cda2cdc7dddc44c89afa5542ec
easonlin win7 <easonlin404@gmail.com>
2016-02-02 11:54:19 +0800

    update

--- spring-web/src/test/java/spittr/web/HomeControllerTest.java
+++ spring-web/src/test/java/spittr/web/HomeControllerTest.java
@@ -1,15 +1,48 @@
 package spittr.web;
 
-import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.view.InternalResourceView;
+
+import spittr.Spittle;
 
 public class HomeControllerTest {
 
 	@Test
 	public void testHomePage() throws Exception {
 		HomeController controller = new HomeController();
-		assertEquals("home", controller.home());
+		MockMvc mockMvc = standaloneSetup(controller).build();
+		mockMvc.perform(get("/")) // Perform GET /
+				.andExpect(view().name("home")); // Expect home view
+	}
+
+	@Test
+	public void shouldShowRecentSpittles() throws Exception {
+		List<Spittle> expectedSpittles = createSpittleList(20);
+
+		// Mock repository
+		SpittleRepository mockRepository = mock(SpittleRepository.class);
+		when(mockRepository.findSpittles(Long.MAX_VALUE, 20)).thenReturn(expectedSpittles);
+
+		SpittleController controller = new SpittleController(mockRepository);
+		MockMvc mockMvc = standaloneSetup(controller)
+				.setSingleView(new InternalResourceView("/WEB-INF/views/spittles.jsp")).build();
+	}
+
+	private List<Spittle> createSpittleList(int count) {
+		List<Spittle> spittles = new ArrayList<Spittle>();
+		for (int i = 0; i < count; i++) {
+			spittles.add(new Spittle("Spittle " + i, new Date()));
+		}
+		return spittles;
 	}
 
 }
