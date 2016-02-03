3a333294757fb11a3590c93c45f510f78dfa9078
easonlin win7 <easonlin404@gmail.com>
2016-02-03 15:44:38 +0800

    update

--- spring-web/src/main/java/spittr/data/Spitter.java
+++ spring-web/src/main/java/spittr/data/Spitter.java
@@ -2,13 +2,26 @@ package spittr.data;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public class Spitter {
 	private long id;
 	
+	@NotNull
+	@Size(min=5, max=16)
 	private String username;
+	
+	@NotNull
+	@Size(min=5, max=25)
 	private String password;
+	
+	@NotNull
+	@Size(min=2, max=30)
 	private String firstName;
+	
+	@NotNull
+	@Size(min=2, max=30)
 	private String lastName;
 	
 	
--- spring-web/src/main/java/spittr/web/SpitterController.java
+++ spring-web/src/main/java/spittr/web/SpitterController.java
@@ -3,9 +3,12 @@ package spittr.web;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -33,7 +36,11 @@ public class SpitterController {
 	}
 
 	@RequestMapping(value = "/register", method = POST)
-	public String processRegistration(Spitter spitter) {
+	public String processRegistration(@Valid Spitter spitter, Errors errors) {
+		if (errors.hasErrors()) {
+			return "registerForm";
+		}
+		
 		spitterRepository.save(spitter);
 		return "redirect:/spitter/" + spitter.getUsername();
 	}
